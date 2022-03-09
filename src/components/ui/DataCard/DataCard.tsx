6993645fe7769db75b17e71ef588b2f3583c0e07
kryban93 <kryban1993@gmail.com>
2022-03-09 22:46:57 +0100

    Added preview more to DataCard Component

--- src/components/ui/DataCard/DataCard.tsx
+++ src/components/ui/DataCard/DataCard.tsx
@@ -1,47 +1,71 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import IconComponent from '../IconComponent/IconComponent';
 import icons from '../../../assets/icons';
 import Button from '../Button/Button';
 
-export default function DataCard({ title = '', created, id }: DataCardProps) {
+export default function DataCard({
+	title = '',
+	created,
+	id,
+	data,
+}: DataCardProps) {
+	const [isPreviewMoreVisible, setPreviewMoreVisibleState] =
+		useState<boolean>(false);
+
 	const handleOpenDetails = () => {};
+	const handlePreviewMore = () => {
+		setPreviewMoreVisibleState(!isPreviewMoreVisible);
+	};
 	return (
 		<StyledWrapper id={id}>
-			<StyledHeading>{title}</StyledHeading>
-			<StyledIconContainer>
-				<Button
-					type="button"
-					iconOnly
-					variant="primary"
-					onClick={() => handleOpenDetails()}
-				>
-					<IconComponent src={icons.chart} alt="go to details" size={30} />
-				</Button>
-				<Button
-					type="button"
-					iconOnly
-					variant="primary"
-					onClick={() => handleOpenDetails()}
-				>
-					<IconComponent src={icons.trash} alt="delete data" size={30} />
-				</Button>
-				<Button
-					type="button"
-					iconOnly
-					variant="primary"
-					onClick={() => handleOpenDetails()}
-				>
-					<IconComponent src={icons.caret} alt="more info icon" size={30} />
-				</Button>
-			</StyledIconContainer>
+			<StyledContentWrapper>
+				<StyledHeading>{title}</StyledHeading>
+				<StyledIconContainer>
+					<Button
+						type="button"
+						iconOnly
+						variant="primary"
+						onClick={() => handleOpenDetails()}
+					>
+						<IconComponent src={icons.chart} alt="go to details" size={30} />
+					</Button>
+					<Button
+						type="button"
+						iconOnly
+						variant="primary"
+						onClick={() => handleOpenDetails()}
+					>
+						<IconComponent src={icons.trash} alt="delete data" size={30} />
+					</Button>
+					<Button
+						type="button"
+						iconOnly
+						variant="primary"
+						onClick={() => handlePreviewMore()}
+					>
+						<IconComponent src={icons.caret} alt="more info icon" size={30} />
+					</Button>
+				</StyledIconContainer>
+			</StyledContentWrapper>
+
+			{isPreviewMoreVisible && (
+				<StyledDataWrapper>
+					{data.map(({ value, unit }) => (
+						<p>
+							{value}
+							{unit}
+						</p>
+					))}
+				</StyledDataWrapper>
+			)}
 		</StyledWrapper>
 	);
 }
 
 const StyledWrapper = styled.div`
 	display: flex;
-	align-items: center;
-	gap 30px;
+	flex-direction: column;
 	background-color: ${({ theme }) => theme.secondary};
 	padding: 10px 20px;
 	border-radius: 3px;
@@ -52,9 +76,16 @@ const StyledIconContainer = styled.div`
 	gap: 10px;
 `;
 
+const StyledContentWrapper = styled.div`
+display: flex;
+	align-items: center;
+	gap 30px;`;
+
 const StyledHeading = styled.h3`
 	width: 150px;
 `;
+
+const StyledDataWrapper = styled.div``;
 
 export type DataCardProps = {
 	title: string;
---
+++ src/components/ui/DetailedHabit/DetailedHabit.tsx
@@ -0,0 +1,9 @@
+export default function DetailedHabit({}: DetailedHabitProps) {
+	return (
+		<div>
+			<p>hello</p>
+		</div>
+	);
+}
+
+export type DetailedHabitProps = {};
